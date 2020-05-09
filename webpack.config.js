a92d3d23aa8ae065bcb9ca668ce45f0714fd898f
Richie Carey <richiecarey@gmail.com>
2020-05-09 19:46:27 -0400

    Add webpack.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  watch: true,
+  entry: "./src/js/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
