ed1f859e4f2505ece538fe9db86f85e5ca7e4c24
Richie Carey <richiecarey@gmail.com>
2020-05-10 18:50:56 -0400

    Updates.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
 "use strict";
-const { src, dest, watch } = require("gulp");
+
+const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass");
+const server = require("browser-sync").create();
+
+function serve() {
+  server.init({
+    server: {
+      baseDir: "./docs",
+    },
+  });
+}
+
+function reload() {
+  server.reload();
+}
+
 function compileSass(done) {
   src("docs/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("docs/css"));
+    .pipe(dest("docs/css"))
+    .pipe(reload);
   done();
 }
+
 function watchSass() {
   watch("docs/scss/**/*.scss", compileSass);
 }
-exports.watchSass = watchSass;
+
+exports.default = parallel(watchSass, serve);
